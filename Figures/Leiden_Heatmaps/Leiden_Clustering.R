21ece72ac6293ee7b067e1199e4fd7ceab0abb6f
Luan Gardenalli <119911744+LuanGardenalli@users.noreply.github.com>
2026-06-03 00:36:54 +0200

    Update Leiden_Clustering.R
    Tidying up the code: dealing with redundant repetitions

--- Figures/Leiden_Heatmaps/Leiden_Clustering.R
+++ Figures/Leiden_Heatmaps/Leiden_Clustering.R
@@ -1,52 +1,32 @@
-#kNN graph construction and Leiden clustering code: 
-
-library(dbscan)
-library(igraph)
-
 #EXP:
-res <- 0.3
-K <- 20
-
-knn <- kNN(heatmap.df[2:19], k = K)
-
-edges <- data.frame(
-  from = rep(1:nrow(heatmap.df[2:19]), each = K),
-  to = as.vector(t(knn$id)))
-
-knn_graph <- graph_from_data_frame(edges, directed = FALSE)
-Leiden <- cluster_leiden(knn_graph, resolution = res)
-
-clusters <- Leiden$membership
-heatmap.df$Cluster <- as.factor(clusters)
+# input: heatmap.df[2:19] (Variable from the heatmapping scripts)
+# res: 0.3
+# K: 20
 
 #PISA:
-res <- 0.1
-K <- 100
-
-knn <- kNN(heatmap.df[2:13], k = K)
-
-edges <- data.frame(
-  from = rep(1:nrow(heatmap.df[2:13]), each = K),
-  to = as.vector(t(knn$id)))
-
-knn_graph <- graph_from_data_frame(edges, directed = FALSE)
-Leiden <- cluster_leiden(knn_graph, resolution = res)
-
-clusters <- Leiden$membership
-heatmap.df$Cluster <- as.factor(clusters)
+# input: heatmap.df[2:13]
+# res: 0.1
+# K: 100
 
 #REX:
-res <- 0.2
-K <- 25
+# input: heatmap.df[2:19]
+# res: 0.2
+# K: 25
 
-knn <- kNN(heatmap.df[2:19], k = K)
-
-edges <- data.frame(
-  from = rep(1:nrow(heatmap.df[2:19]), each = K),
-  to = as.vector(t(knn$id)))
-
-knn_graph <- graph_from_data_frame(edges, directed = FALSE)
-Leiden <- cluster_leiden(knn_graph, resolution = res)
-
-clusters <- Leiden$membership
-heatmap.df$Cluster <- as.factor(clusters)
+Leiden_clustering <- function(res,
+                              K,
+                              input){
+  library(dbscan)
+  library(igraph)
+  
+  knn <- kNN(input, k = K)
+  
+  edges <- data.frame(
+    from = rep(1:nrow(input), each = K),
+    to = as.vector(t(knn$id)))
+  
+  knn_graph <- graph_from_data_frame(edges, directed = FALSE)
+  Leiden <- cluster_leiden(knn_graph, resolution = res)
+  
+  clusters <- Leiden$membership
+}
